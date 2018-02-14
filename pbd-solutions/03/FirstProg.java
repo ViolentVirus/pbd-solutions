94e36172bffcca5e9e8afefeb238a7fecc82c804
ViolentVirus <36419608+ViolentVirus@users.noreply.github.com>
2018-02-14 14:33:35 -0500

    Add files via upload

---
+++ pbd-solutions/03/FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+    public static void main(String[] args) {
+        System.out.println("Abtin is bad at life!");
+        System.out.println("Why you ask?");
+        System.out.println("Because its Abtin for fucks sake!!!!");
+    }
+}
---
+++ pbd-solutions/06/LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself
+{
+    public static void main( String[] args )
+    {
+        System.out.println("+---------------------------------------------------------+");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                         |");
+        System.out.println("|                                                         |");
+        System.out.println("|                              Farbod Tajik               |");
+        System.out.println("|                              54 Aileen Rd               |");
+        System.out.println("|                              Thornhill, ON L3T 5T1      |");
+        System.out.println("|                                                         |");
+        System.out.println("+---------------------------------------------------------+");
+    }
+}
