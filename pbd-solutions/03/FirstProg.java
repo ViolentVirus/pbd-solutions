53deee00e528b23a3c7ed66a33b5f7f5881b1a1a
Farbod Tajik <36419608+ViolentVirus@users.noreply.github.com>
2018-02-15 13:14:33 -0500

    Update FirstProg.java

--- pbd-solutions/03/FirstProg.java
+++ pbd-solutions/03/FirstProg.java
@@ -2,6 +2,6 @@ public class FirstProg {
     public static void main(String[] args) {
         System.out.println("Abtin is bad at life!");
         System.out.println("Why you ask?");
-        System.out.println("Because its Abtin for fucks sake!!!!");
+        System.out.println("Because its Abtin!!!");
     }
 }
